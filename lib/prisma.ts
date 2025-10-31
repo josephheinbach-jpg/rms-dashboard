5a9e69843250346ad9d1e4d7fbf630c42206c874
josephheinbach-jpg <241217869+josephheinbach-jpg@users.noreply.github.com>
2025-10-31 10:03:24 -0400

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,18 @@
-// lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient }
+// ✅ Temporary fallback for DATABASE_URL missing in Vercel
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL =
+    'postgresql://postgres:0t9HYmnNwnyzXEgh@db.aozigwmupsuospytwlas.supabase.co:5432/postgres'
+  console.warn('⚠️ DATABASE_URL fallback in use (temporary)')
+}
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['warn', 'error'],
+    log: ['error', 'warn'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
