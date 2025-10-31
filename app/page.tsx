d7d752ad043a868a140e71bf3c45695434abb5a6
josephheinbach-jpg <241217869+josephheinbach-jpg@users.noreply.github.com>
2025-10-30 22:43:44 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 import { prisma } from '../lib/prisma'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
