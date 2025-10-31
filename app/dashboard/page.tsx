e2bcc5d31e27f85434021a0ac1e13505f5f8357a
josephheinbach-jpg <241217869+josephheinbach-jpg@users.noreply.github.com>
2025-10-30 22:44:10 -0400

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 import { requireOrg } from '../../lib/authz'
 import { prisma } from '../../lib/prisma'
 import Link from 'next/link'
